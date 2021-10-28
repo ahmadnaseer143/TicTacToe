1b5ed4e37dc6af8223c55d38b4f3156b1b52fb5a
ahmadnaseer143 <anaseer942@gmail.com>
2021-10-28 11:24:48 +0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const BB = (props) => {
+  const [value, setValue] = useState()
+  const [isPress, setisPress] = useState(false)
+  const handlePress = () => {
+    if (!isPress) {
+      if (props.player) {
+        setValue("X")
+        props.changePlayer()
+      }
+      else {
+        setValue("0")
+        props.changePlayer()
+      }
+      setisPress(true)
+    }
+
+  }
+
+  return (
+    <TouchableOpacity style={{ padding: 20, backgroundColor: "black", margin: 10 }} onPress={handlePress}>
+      <Text style={{ color: "white" }}>{value}</Text>
+    </TouchableOpacity>
+
+  )
+}
 
 export default function App() {
+  
+  
+  const [player, setPlayer] = useState(true)
+  const changePlayer= ()=>{
+    setPlayer(!player)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Player 2: X</Text>
+      {/* first line */}
+      <View style={styles.line}>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+      </View>
+      {/* second line */}
+      <View style={styles.line}>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+      </View>
+      
+      {/* third line */}
+      <View style={styles.line}>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+        <BB changePlayer={changePlayer} player={player}/>
+      </View>
+      <Text>Player 1: 0</Text>
     </View>
   );
 }
@@ -17,5 +68,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  line: {
+    flexDirection: "row",
+    paddingBottom: 10,
   },
 });
